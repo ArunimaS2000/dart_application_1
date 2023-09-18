88b83e83abdc06ee9d07249bc3500fcd4992bea8
ArunimaS <mail.arunimaS@123>
2023-09-18 12:49:30 +0530

    first commit

---
+++ bin/datatype.dart
@@ -0,0 +1,9 @@
+void main(){
+Stringname = 'ammu';
+ int  age = 20;
+ int phone = 8921292190;
+ double cgpa = 8.1;
+ string  email = 'ammu@gmail.com';
+
+
+}
---
+++ bin/first_sample.dart
@@ -0,0 +1,6 @@
+void main() {
+  print('my name is arunima');
+  print(23);
+  print('my phone number is 8921292190');
+  print(8.2);
+}
